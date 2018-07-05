88ce986e5bca2b2fe77d00e3dd5fc4b6b3aa3d7e
Dimitris Balaouras <dbalaouras@gmail.com>
2018-07-05 17:51:29 +0300

    Add support for GELF logging

---
+++ webhawk/services/log_filters.py
@@ -0,0 +1,17 @@
+import logging
+
+
+class StaticFieldFilter(logging.Filter):
+    """
+    Python logging filter that adds the given static contextual information
+    in the ``fields`` dictionary to all logging records.
+    """
+
+    def __init__(self, fields):
+        super(StaticFieldFilter, self).__init__()
+        self.static_fields = fields
+
+    def filter(self, record):
+        for k, v in self.static_fields.items():
+            setattr(record, k, v)
+        return True
