a02752060e6c6db1d7ba8e2c76418b53669ed99d
Dimitris Balaouras <dbalaouras@gmail.com>
2019-10-12 15:17:14 +0300

    Add support or Gitlab webhooks

--- webhawk/rest/gitlab_webhook.py
+++ webhawk/rest/gitlab_webhook.py
@@ -1,3 +1,4 @@
+from rest.api_errors import InvalidUsage
 from rest.github_webhook import GithubWebHook
 
 __author__ = "Dimi Balaouras"
@@ -10,3 +11,25 @@ class GitlabWebHook(GithubWebHook):
     RESTful Resource for the Gitlab Webhook
     """
     resource_name = "gitlab"
+
+    def create_task(self, payload):
+        """
+        Creates a new build task using the input (usually a POST Payload)
+        :param payload: The task payload
+        :return: The newly created task
+        """
+
+        # Get the task manager
+        task_manager = self._context.get("task_manager")
+
+        try:
+            repository_name = payload['repository']['name']
+            branch_name = payload['ref'].split('/')[2]
+            vcs = "git"
+        except KeyError:
+            raise InvalidUsage("Invalid payload: %s" % str(payload))
+
+        # Construct the new task
+        new_task = task_manager.create_new_task(repository_name=repository_name, branch_name=branch_name, vcs=vcs)
+
+        return new_task
